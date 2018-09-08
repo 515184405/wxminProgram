a063e96cfd3cdfbac577ff38b5cd46748759c69f
fengyunlong <297028817@qq.com>
2018-09-08 15:22:22 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,12 +6,7 @@ Page({
   data: {
     // 推荐套系
     recommend: [],
-    navArr:[
-      { title: "客 照", image: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4236226554,3005486819&fm=27&gp=0.jpg',openType:'switchTab',url:'/pages/case/case' },
-      { title: '套 系', image: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3931882706,576426697&fm=27&gp=0.jpg', openType: 'navigateTo', url: '/pages/department/department'},
-      { title: '活 动', image: 'http://m.only1314.com/uploadfile/2018/0815/20180815012601292.jpg', openType: 'navigateTo', url: '/pages/activity/activity' },
-      { title: '关于我们', image: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1610349391,986729414&fm=27&gp=0.jpg', openType: 'navigateTo', url: '/pages/aboutUs/aboutUs' }
-    ], 
+    navArr:[], 
     scene: [],
     imgUrls: [],
     footer:{},
@@ -27,6 +22,7 @@ Page({
     var that = this;
     this.setData({ store_name: app.globalData.store_name })
     this.getimgUrls();
+    this.daohang();
     this.scene();
     app.recommend(this);
     app.footer(this);
@@ -78,6 +74,20 @@ Page({
     app._Get('index/get_data', 'm/anli/w__is_index/1/f/id,title,price,imgs,remark as dress/o/sort@asc/l/3', function (data) {
       //   console.log(data);
       that.setData({ scene: data })
+    })
+  },
+  //导航
+  daohang: function () {
+    var that = this;
+    app._Get('index/get_data', 'm/daohang/f/id,title,url,img', function (data) {
+       for(var i in data){
+         var k = data[i].url.split("@");
+         data[i].url = k[0];
+         data[i].openType = k[1];
+         data[i].image = data[i].img;
+       }
+        // console.log(data);
+      that.setData({ navArr: data })
     })
   },
   goto:function(e){
